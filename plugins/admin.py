38be30c3cced99c14242921eb55e3f170e29b683
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-07-01 23:01:02 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,54 +1,50 @@
-import pyrogram
+from pyrogram import Client, filters, enums
 import datetime
-import random
-import time
-import pymongo
 import pytz
+import asyncio
 
-bot = pyrogram.Client("my_bot", api_id="15428219", api_hash="0042e5b26181a1e95ca40a7f7c51eaa7", bot_token="5166769555:AAFM8gtzAOJ4H9MRteci8QSvjO4f6m8YTCc")
+@Client.on_message((filters.command(["report"]) | filters.regex("@admins") | filters.regex("@admin")) & filters.group)
+async def notify_admin(bot, message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    administrators = []
+    chat_member = await bot.get_chat_member(chat_id=chat_id, user_id=user_id)
+    if (
+            chat_member.status == enums.ChatMemberStatus.ADMINISTRATOR
+            or chat_member.status == enums.ChatMemberStatus.OWNER
+    ):
+        return await message.delete()
+    async for m in bot.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+        administrators.append(m)
+    full_name = message.from_user.first_name + " " + message.from_user.last_name if message.from_user.last_name else message.from_user.first_name
+    
+    ist = pytz.timezone("Asia/Kolkata")
+    report_time = datetime.datetime.now(pytz.utc).astimezone(ist).strftime("%I:%M:%S %p")
+    report_date = datetime.datetime.now(pytz.utc).astimezone(ist).strftime("%d-%B-%Y")
+    report_day = datetime.datetime.now(pytz.utc).astimezone(ist).strftime("%A")
 
-db = pymongo.MongoClient("mongodb+srv://RPN:RPN@tgreporternew.rys1amm.mongodb.net/?retryWrites=true&w=majority").my_db
+    reply_message = f"<b><i>✅ Rᴇᴩᴏʀᴛ Sᴇɴᴅ Sᴜᴄᴄᴇꜱꜱꜰᴜʟʟ ✅</i></b>\n\n"
+    reply_message += f"<b>👤 Rᴇᴘᴏʀᴛᴇᴅ ᴜsᴇʀ: {message.from_user.username}\n"
+    reply_message += f"🆔 Rᴇᴘᴏʀᴛᴇᴅ ᴜsᴇʀ ɪᴅ: {message.from_user.id}\n"
+    reply_message += f"📝 Rᴇᴘᴏʀᴛ ᴛʀᴀᴄᴋ ɪᴅ: [#TG8836467]({message.link})\n\n"
+    reply_message += f"💬 Rᴇᴘᴏʀᴛ ᴛᴇxᴛ: {message.reply_to_message.text if message.reply_to_message else message.text.split(' ', 1)[1]}\n\n"
+    reply_message += f"⏲️ Rᴇᴘᴏʀᴛ ᴛɪᴍᴇ: {report_time}\n"
+    reply_message += f"🗓️ Rᴇᴘᴏʀᴛ ᴅᴀᴛᴇ: {report_date}\n"
+    reply_message += f"⛅ Rᴇᴘᴏʀᴛ ᴅᴀʏ: {report_day}</b>"
 
-@bot.on_message()
-async def handle_message(client, message):
-    if message.text.startswith("@admin"):
-        # Send the loading message
-        loading_message = await message.reply("Report sending ○○○○○○○○○")
-
-        report_text = message.text[6:]
-
-        # Get the current time in India
-        india_timezone = pytz.timezone('Asia/Kolkata')
-        now_in_india = datetime.datetime.now(india_timezone)
-        report_time = now_in_india.strftime('%I:%M:%S %p')
-        report_date = now_in_india.strftime('%d-%m-%Y')
-        report_day = now_in_india.strftime('%A')
-
-        track_id = f"#MB{random.randint(1, 1000000)}"
-        report_top = "✅ Rᴇᴘᴏʀᴛ sᴇɴᴅ ᴛᴏ ᴀᴅᴍɪɴ ✅"
-
-        report = {
-            "report_top": report_top,
-            "reporter": message.from_user.first_name,
-            "reporter_id": message.from_user.id,
-            "track_id": track_id,
-            "report_text": report_text,
-            "report_time": report_time,
-            "report_date": report_date,
-            "report_day": report_day,
-        }
-        db.reports.insert_one(report)
-
-        # Update the loading message with the filled animation
-        for i in range(3):
-            filled = "●" * (i + 1)
-            unfilled = "○" * (3 - (i + 1))
-            loading_bar = f"Report sending {filled}{unfilled}"
-            await loading_message.edit_text(loading_bar)
-            time.sleep(0.5)
-
-        await loading_message.delete()
-
-        await message.reply(f"{report['report_top']}\n\n👤 Rᴇᴘᴏʀᴛᴇʀ: {report['reporter']}\n🆔 Rᴇᴘᴏʀᴛᴇʀ ɪᴅ: {report['reporter_id']}\n📜 Tʀᴀᴄᴋ ɪᴅ: {report['track_id']}\n\n💬 Rᴇᴘᴏᴛʀ ᴛᴇxᴛ : {report['report_text']}\n\n⌚ Rᴇᴘᴏʀᴛ ᴛɪᴍᴇ: {report['report_time']}\n🗓️ Rᴇᴘᴏʀᴛ ᴅᴀᴛᴇ: {report['report_date']}\n⛅ Rᴇᴘᴏʀᴛ ᴅᴀʏ: {report['report_day']}")
-        channel_id = -1001904370879
-        await client.send_message(channel_id, f"Reporter: {report['reporter']}\nReporter ID: {report['reporter_id']}\nTrack ID: {report['track_id']}\nReport Text: {report['report_text']}\nReport Time: {report['report_time']}\nReport Date: {report['report_date']}\nReport Day: {report['report_day']}")
+    report = message.reply_to_message if message.reply_to_message else message
+    m = await message.reply_text(reply_message, disable_web_page_preview=True)
+    await asyncio.sleep(10)
+    await m.delete()
+    await message.delete()
+    for admin in administrators:
+        try:
+            if admin.user.id != message.from_user.id:
+                await bot.send_message(
+                    chat_id=admin.user.id, 
+                    text=f"⚠️ ATTENTION!\n\n<a href=tg://user?id={user_id}>{full_name}</a> Hᴀꜱ Rᴇǫᴜɪʀᴇᴅ Aɴ Aᴅᴍɪɴ Aᴄᴛɪᴏɴ Iɴ Tʜᴇ Gʀᴏᴜᴘ: {message.chat.title}\n\n[👉🏻 Go to message]({message.link})",
+                    disable_web_page_preview=True
+                )
+        except:
+            pass
+            pass
