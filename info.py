7c6d0d1b06e52162071d653fb24e450fc9e5da3c
Soul pc <119080077+soulpc@users.noreply.github.com>
2024-02-07 10:41:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -79,25 +79,6 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'raixchat')
-P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), False)
-IMDB = is_enabled((environ.get('IMDB', "False")), True)
-AUTO_FFILTER = is_enabled((environ.get('AUTO_FFILTER', "True")), True)
-AUTO_DELETE = is_enabled((environ.get('AUTO_DELETE', "True")), True)
-SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", '📂 <b>File Name</b>: <code>{file_name}</code> \n\n ⚙️ Fɪʟᴇ Sɪᴢᴇ : {file_size} \n\n╔══ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ═══╗\n♻️ <b>𝙅𝙊𝙄𝙉:-</b> [𝗚𝗥𝗢𝗨𝗣 𝗟𝗜𝗡𝗞](https://t.me/ARAKAL_THERAVAD_MOVIES) \n♻️ <b>𝙅𝙊𝙄𝙉:-</b> [𝗢𝗧𝗧 𝗨𝗣𝗗𝗔𝗧𝗘𝗦](https://t.me/OTT_ARAKAL_THERAVAD_MOVIES) \n♻️ <b>𝙅𝙊𝙄𝙉:-</b> [𝗖𝗛𝗔𝗡𝗡𝗘𝗟  𝗟𝗜𝗡𝗞](https://t.me/ARAKAL_THERAVAD_GROUP_LINKS) \n╚══ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ═══╝</b>')
-BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", '')
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", '🏷 𝖳𝗂𝗍𝗅𝖾: <a href={url}>{title}</a> \n🔮 𝖸𝖾𝖺𝗋: {year} \n⭐️ 𝖱𝖺𝗍𝗂𝗇𝗀𝗌: {rating}/ 10  \n🎭 𝖦𝖾𝗇𝖾𝗋𝗌: {genres} \n\n🎊 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 [[ARAKAL THERAVAD MOVIES]](https://t.me/ARAKAL_THERAVAD_MOVIES)')
-LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
-SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
-MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
-INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
-MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "False")), False)
-PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
-PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True)
-
-
-
 
 # _______________________________________________________________________________________________________________ #
 
