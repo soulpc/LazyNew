24b7bea970860dfbf36d632008067ba19a5d65c5
Soul pc <119080077+soulpc@users.noreply.github.com>
2024-02-07 10:22:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -64,7 +64,7 @@ LOG_CHANNEL = int(environ.get('LOG_CHANNEL', "-1002108573842"))
 
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'MovieBossTG')
 
-P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
+P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 
 IMDB = is_enabled((environ.get('IMDB', "False")), True)
 
